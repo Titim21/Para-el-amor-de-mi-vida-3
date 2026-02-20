cf2630ea5eb7d961c156d5dade528516dbf3fa3b
Titim21 <sarmientor635@gmail.com>
2026-02-19 23:47:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Reveal al hacer scroll
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
